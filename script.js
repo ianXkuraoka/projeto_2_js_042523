cf61e5d2ac36dda6047873291e51de0eed476d01
Ian <ian.xk9@gmail.com>
2023-04-25 11:21:12 -0300

    quick_save_2

--- script.js
+++ script.js
@@ -14,8 +14,21 @@ for(let i=0; i<Users; i++){
     msg.innerHTML +=`<p>${usuarios[i].nome} ${usuarios[i].idade}</p>`
 }
 
+
+/*criando um array de imagens*/
 var imagens=[
     'img/1.jpg',
     'img/2.jpg',
     'img/3.jpg',
 ];
+
+
+/*criando um array de variáveis*/
+var index =0;
+var time =2000;
+
+
+/*criando um array de função*/
+function slideShow(){
+    document.getElementById('image')
+}
